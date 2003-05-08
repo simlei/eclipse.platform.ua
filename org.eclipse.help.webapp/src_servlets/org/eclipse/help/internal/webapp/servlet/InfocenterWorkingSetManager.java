daa1fe8c6b18a6fc1ee88418902d3da71fd0d82f
Konrad Kolosowski <kkolosow>
2003-05-08 23:29:15 +0000

    infocenter working sets prototype

--- org.eclipse.help.webapp/src_servlets/org/eclipse/help/internal/webapp/servlet/InfocenterWorkingSetManager.java
+++ org.eclipse.help.webapp/src_servlets/org/eclipse/help/internal/webapp/servlet/InfocenterWorkingSetManager.java
@@ -11,13 +11,13 @@
 package org.eclipse.help.internal.webapp.servlet;
 
 import java.io.*;
-import java.net.*;
 import java.util.*;
 
 import javax.servlet.http.*;
 
 import org.eclipse.core.runtime.*;
 import org.eclipse.help.internal.*;
+import org.eclipse.help.internal.util.*;
 import org.eclipse.help.internal.workingset.*;
 
 /**
@@ -26,8 +26,10 @@ import org.eclipse.help.internal.workingset.*;
  * @since 3.0
  */
 public class InfocenterWorkingSetManager implements IHelpWorkingSetManager {
-	private static final int MAX_COOKIES=15;
-	 private static final int MAX_COOKIE_PAYLOAD = 4096 - "wset01=".length()  - "81920<".length()- 1; // Http11 connector will not serve the request that contains line longer than 4096 (not even one full cookie)
+	private static final String COOKIE_NAME = "wset";
+	private static final int MAX_COOKIES = 15;
+	private static final int MAX_COOKIE_PAYLOAD =
+		4096 - "wset01=".length() - "81920<".length() - 1;
 
 	private HttpServletRequest request;
 	private HttpServletResponse response;
@@ -115,108 +117,107 @@ public class InfocenterWorkingSetManager implements IHelpWorkingSetManager {
 	}
 
 	private void restoreState() {
-		String data = restoreString();
+		String data = CookieUtil.restoreString(COOKIE_NAME, request);
 		if (data == null) {
 			return;
 		}
 
-		String[] values = data.split("\\|", -1);
+		//String[] values = data.split("\\|", -1);
+		String[] values = CookieUtil.split(data, '|');
 		if (values.length < 1) {
 			return;
 		}
-		try {
-			currentWorkingSet = URLDecoder.decode(values[0], "UTF8");
-		} catch (UnsupportedEncodingException uee) {
-		}
+		currentWorkingSet = URLCoder.decode(values[0] /*, "UTF8"*/
+		);
+		i : for (int i = 1; i < values.length; i++) {
+			//String[] nameAndHrefs = values[i].split("&", -1);
+			String[] nameAndHrefs = CookieUtil.split(values[i], '&');
 
-		try {
-			i : for (int i = 1; i < values.length; i++) {
-				String[] nameAndHrefs = values[i].split("&", -1);
+			String name = URLCoder.decode(nameAndHrefs[0] /*, "UTF8"*/
+			);
 
-				String name = URLDecoder.decode(nameAndHrefs[0], "UTF8");
-
-				AdaptableHelpResource[] elements =
-					new AdaptableHelpResource[nameAndHrefs.length - 1];
-				// for each href (working set resource)
-				for (int e = 0; e < nameAndHrefs.length - 1; e++) {
-					int h = e + 1;
+			AdaptableHelpResource[] elements =
+				new AdaptableHelpResource[nameAndHrefs.length - 1];
+			// for each href (working set resource)
+			for (int e = 0; e < nameAndHrefs.length - 1; e++) {
+				int h = e + 1;
+				elements[e] = getAdaptableToc(URLCoder.decode(nameAndHrefs[h]
+				/*, "UTF8"*/
+				));
+				if (elements[e] == null) {
 					elements[e] =
-						getAdaptableToc(
-							URLDecoder.decode(nameAndHrefs[h], "UTF8"));
-					if (elements[e] == null) {
-						elements[e] =
-							getAdaptableTopic(
-								URLDecoder.decode(nameAndHrefs[h], "UTF8"));
-					}
-					if (elements[e] == null) {
-						// working set cannot be restored
-						System.out.println(
-							"cannot restore ws=" + nameAndHrefs[h]);
-						// TODO comment system.out
-						continue i;
-					}
+						getAdaptableTopic(URLCoder.decode(nameAndHrefs[h]
+					/*, "UTF8"*/
+					));
 				}
-				WorkingSet ws = createWorkingSet(name, elements);
-				workingSets.add(ws);
+				if (elements[e] == null) {
+					// working set cannot be restored
+					System.out.println("cannot restore ws=" + nameAndHrefs[h]);
+					// TODO comment system.out
+					continue i;
+				}
 			}
-		} catch (UnsupportedEncodingException uee) {
+			WorkingSet ws = createWorkingSet(name, elements);
+			workingSets.add(ws);
 		}
 
 	}
-	/* * Persists all working sets. Should only be called by the webapp working
+	/* *
+	 *  Persists all working sets. Should only be called by the webapp working
 	 * set dialog.
-	 * Saves the working sets in the persistence store
+	 * Saves the working sets in the persistence store (cookie)
 	 * format: curentWorkingSetName|name1&href11&href12|name2&href22
 	 */
-	private void saveState() {
-		StringBuffer data = new StringBuffer();
+	private void saveState() /* throws IOException*/ {
 		try {
-			data.append(URLEncoder.encode(currentWorkingSet, "UTF8"));
-		} catch (UnsupportedEncodingException uee) {
-		}
+			StringBuffer data = new StringBuffer();
+			data.append(URLCoder.encode(currentWorkingSet /*, "UTF8"*/
+			));
 
-		for (Iterator i = workingSets.iterator(); i.hasNext();) {
-			data.append('|');
-			WorkingSet ws = (WorkingSet) i.next();
-			try {
-				data.append(URLEncoder.encode(ws.getName(), "UTF8"));
-			} catch (UnsupportedEncodingException uee) {
-			}
+			for (Iterator i = workingSets.iterator(); i.hasNext();) {
+				data.append('|');
+				WorkingSet ws = (WorkingSet) i.next();
+				data.append(URLCoder.encode(ws.getName() /*, "UTF8"*/
+				));
 
-			AdaptableHelpResource[] resources = ws.getElements();
-			for (int j = 0; j < resources.length; j++) {
-				data.append('&');
+				AdaptableHelpResource[] resources = ws.getElements();
+				for (int j = 0; j < resources.length; j++) {
+					data.append('&');
 
-				IAdaptable parent = resources[j].getParent();
-				if (parent == getRoot()) {
-					// saving toc
-					try {
-						data.append(
-							URLEncoder.encode(resources[j].getHref(), "UTF8"));
-					} catch (UnsupportedEncodingException uee) {
-					}
-				} else {
-					// saving topic as tochref_topic#_
-					AdaptableToc toc = (AdaptableToc) parent;
-					AdaptableHelpResource[] siblings = (toc).getChildren();
-					for (int t = 0; t < siblings.length; t++) {
-						if (siblings[t] == resources[j]) {
-							try {
-								data.append(
-									URLEncoder.encode(toc.getHref(), "UTF8"));
-							} catch (UnsupportedEncodingException uee) {
+					IAdaptable parent = resources[j].getParent();
+					if (parent == getRoot()) {
+						// saving toc
+						data.append(URLCoder.encode(resources[j].getHref()
+						/*, "UTF8"*/
+						));
+					} else {
+						// saving topic as tochref_topic#_
+						AdaptableToc toc = (AdaptableToc) parent;
+						AdaptableHelpResource[] siblings = (toc).getChildren();
+						for (int t = 0; t < siblings.length; t++) {
+							if (siblings[t] == resources[j]) {
+								data.append(URLCoder.encode(toc.getHref()
+								/*, "UTF8"*/
+								));
+								data.append('_');
+								data.append(t);
+								data.append('_');
+								break;
 							}
-							data.append('_');
-							data.append(t);
-							data.append('_');
-							break;
 						}
 					}
 				}
 			}
-		}
 
-		saveString(data.toString());
+			CookieUtil.saveString(
+				COOKIE_NAME,
+				data.toString(),
+				MAX_COOKIES,
+				request,
+				response);
+		} catch (IOException ioe) {
+			// TODO do not catch
+		}
 	}
 
 	/**
@@ -224,16 +225,6 @@ public class InfocenterWorkingSetManager implements IHelpWorkingSetManager {
 	*/
 	public void workingSetChanged(WorkingSet changedWorkingSet) {
 		saveState();
-	}
-
-	/**
-	 * Synchronizes the working sets. Should only be called by the webapp
-	 * working set manager dialog.
-	 *
-	 * @param changedWorkingSet the working set that has changed
-	 */
-	public void synchronizeWorkingSets() {
-		//HelpSystem.getWorkingSetManager(locale).synchronizeWorkingSets();
 	}
 
 	public AdaptableToc getAdaptableToc(String href) {
@@ -269,131 +260,6 @@ public class InfocenterWorkingSetManager implements IHelpWorkingSetManager {
 				return (AdaptableTopic) topics[index];
 		}
 
-		return null;
-	}
-	/**
-	 * Saves string in browser cookies.  Cookies can store limited length string.
-	 * This method will attemt to split string among multiple cookies (up to 15 out of 20 possible cookies).
-	 * The following cookies will be set
-	 * wset1=length<substing1
-	 * wset2=substrging2
-	 * ...
-	 * wsetn=substringn
-	 * @param data a string containing legal characters for cookie value
-	 * @throws exception when data is too long.
-	 */
-	private void saveString(String data) {
-		int len = data.length();
-		int n = len / MAX_COOKIE_PAYLOAD;
-		if (n >MAX_COOKIES) {
-			// too much data, do not save (there is only 20 cookies per domain)
-			return;
-		}
-		for (int i = 1; i <= n; i++) {
-			Cookie cookie = null;
-			if (i == 1) {
-				cookie =
-					new Cookie(
-						"wset1",
-						data.length() + "<" + data.substring(0, MAX_COOKIE_PAYLOAD));
-			} else {
-				cookie = new Cookie("wset"+i, data.substring(MAX_COOKIE_PAYLOAD*(i-1), MAX_COOKIE_PAYLOAD*i));
-			}
-			cookie.setMaxAge(5 * 365 * 24 * 60 * 60);
-			// TODO comment system.out
-			System.out.println("Saving data in cookie: " + cookie.getValue());
-			response.addCookie(cookie);
-		}
-		if (len % MAX_COOKIE_PAYLOAD > 0) {
-			Cookie cookie = null;
-			if (n == 0) {
-				cookie =
-					new Cookie(
-						"wset1",
-						len+ "<" + data.substring(0, len));
-			} else {
-				cookie = new Cookie("wset"+(n+1), data.substring(MAX_COOKIE_PAYLOAD*n, len));
-			}
-			cookie.setMaxAge(5 * 365 * 24 * 60 * 60);
-			// TODO comment system.out
-			System.out.println("Saving data in cookie: " + cookie.getValue());
-			response.addCookie(cookie);
-			
-		}
-		// if using less cookies than before, delete not needed cookies
-		for(int i=n+1; i<=MAX_COOKIES; i++){
-			if(i==n+1 &&len % MAX_COOKIE_PAYLOAD>0){
-				continue;
-			}
-			Cookie cookie=new Cookie("wset"+i,"");
-			cookie.setMaxAge(0);
-			response.addCookie(cookie);
-
-		}
-		
-	}
-
-	/**
-	 * @return null or String
-	 */
-	private String restoreString() {
-		String value1=getCookieValue("wset1");
-		if(value1==null){
-			return null;
-		}
-		String lengthAndSubstring1[]=value1.split("<");
-		if(lengthAndSubstring1.length<2){
-			return null;
-		}
-		int len=0;
-		try{
-			len=Integer.parseInt(lengthAndSubstring1[0]);
-		}catch(NumberFormatException nfe){
-			return null;
-		}
-		if(len<=0){
-			return null;
-		}
-		StringBuffer data=new StringBuffer(len);
-		data.append(lengthAndSubstring1[1]);
-		int n = len / MAX_COOKIE_PAYLOAD;
-		for (int i = 2; i <= n; i++) {
-			String substring=getCookieValue("wset"+i);
-			if(substring==null){
-				return null;
-			}
-			data.append(substring);
-		}
-		if (len % MAX_COOKIE_PAYLOAD > 0 && n>0) {
-			String substring=getCookieValue("wset"+(n+1));
-			if(substring==null){
-				return null;
-			}
-			data.append(substring);
-		}
-		// TODO comment system.out
-		System.out.println("data.length()"+data.length());
-		if(data.length()!=len){
-			// TODO comment system.out
-			System.out.println("Verification error data lenght is "+data.length()+", instead of "+len);
-		}
-		
-		return data.toString();
-	}
-
-	/**
-	 * @return null or String
-	 */
-	private String getCookieValue(String name) {
-		Cookie[] cookies = request.getCookies();
-		if (cookies == null) {
-			return null;
-		}
-		for (int i = 0; i < cookies.length; i++) {
-			if (name.equals(cookies[i].getName())) {
-				return cookies[i].getValue();
-			}
-		}
 		return null;
 	}
 
